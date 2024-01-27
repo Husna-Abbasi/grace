961f9476d5ffd9d7ace5474ed00111e5675b33f5
husna abbasi <husnaabbasi1999@gmail.com>
2024-01-27 23:04:45 +0500

    First commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+        <div className="bg-white h-[100vh] w-full">
+            <h1 className="text-black">
+                dashboard
+            </h1>
+        </div>
+    )
+}
