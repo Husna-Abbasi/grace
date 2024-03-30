014fce1767fb4ef2bb6a50992dc3c2a3dcdc54c2
husnaabbasi <husnaabbasie@gmail.com>
2024-03-30 22:46:12 +0500

    updated skeleton component

--- components/Skeleton/CardSkeleton.component.js
+++ components/Skeleton/CardSkeleton.component.js
@@ -5,35 +5,30 @@ const CardSkeletonComponent = ({width, height}) => {
     const [active] = useState(true);
     const [size] = useState('small');
     const [avatarShape] = useState('square');
-    return (
-        <div style={{width, height}}>
+    return (<div style={{width, height}}>
             <div className="flex flex-col gap-2 hidden md:block ">
                 <Skeleton.Node
                     active={active}
                     style={{
-                        height: '200px',
-                        width: '300px',
+                        height: '200px', width: '300px',
                     }}/>
                 <div className="flex justify-center items-center flex-col pt-2">
                     <Skeleton.Input
                         active={active}
                         style={{
-                            height: '13px',
-                            width: '300px',
+                            height: '13px', width: '300px',
                         }}/>
                     <Skeleton.Input
                         active={active}
                         style={{
-                            height: '13px',
-                            width: '300px',
+                            height: '13px', width: '300px',
                         }}/>
                 </div>
                 <div className="flex gap-4 justify-around pt-2">
                     <Skeleton.Button
                         active={active}
                         style={{
-                            height: '30px',
-                            width: '1px',
+                            height: '30px', width: '1px',
                         }}/>
                     <Skeleton.Button
                         active={active}
@@ -51,21 +46,18 @@ const CardSkeletonComponent = ({width, height}) => {
                 <Skeleton.Node
                     active={active}
                     style={{
-                        height: '160px',
-                        width: '250px',
+                        height: '160px', width: '250px',
                     }}/>
                 <div className="flex justify-center items-center flex-col pt-2">
                     <Skeleton.Input
                         active={active}
                         style={{
-                            height: '10px',
-                            width: '230px',
+                            height: '10px', width: '230px',
                         }}/>
                     <Skeleton.Input
                         active={active}
                         style={{
-                            height: '10px',
-                            width: '230px',
+                            height: '10px', width: '230px',
                         }}/>
                 </div>
                 <div className="flex gap-1 justify-around">
@@ -87,8 +79,7 @@ const CardSkeletonComponent = ({width, height}) => {
 
                 </div>
             </div>
-        </div>
-    );
+        </div>);
 };
 
 export default CardSkeletonComponent;
