d1c5d95b3b43fc0a3a8d64e09dabb3ac908cd617
husnaabbasi <husnaabbasie@gmail.com>
2024-06-18 23:23:07 +0500

    added component

--- components/Button/Loading-Icon.component.js
+++ components/Button/Loading-Icon.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
-import { Spin } from 'antd';
-const LoadingIconComponent = () => <Spin />;
+import {Spin} from 'antd';
+
+const LoadingIconComponent = () => <Spin/>;
 export default LoadingIconComponent;
