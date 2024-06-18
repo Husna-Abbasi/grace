e1ad07fdf1ad11449ef8a3fdf46ea415860ff7ca
husnaabbasi <husnaabbasie@gmail.com>
2024-06-19 00:24:45 +0500

    added component

--- components/Popup/Popup.component.js
+++ components/Popup/Popup.component.js
@@ -12,7 +12,6 @@ const Popup = ({children, title, showPopup, setShowPopup, showFooter = true}) =>
                         className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                         {/*header*/}
                         <div className="flex  justify-between rounded-t px-10 pt-5 items-center">
-
                             <h3 className="text-sm font-semibold text-gray-400 ">{title}</h3>
                             <button
                                 className="bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
