b3c788cdc985f43531157e422fac33632c190bc1
husnaabbasi <husnaabbasie@gmail.com>
2024-06-21 20:26:23 +0500

    added component

--- components/Popup/Popup.component.js
+++ components/Popup/Popup.component.js
@@ -29,7 +29,8 @@ const Popup = ({children, title, showPopup, setShowPopup, showFooter = true}) =>
                                     {children}
                                 </div>
                                 {/*footer*/}
-                                {showFooter && <div
+                                {showFooter &&
+                                    <div
                                     className="flex items-center justify-end p-4 border-t border-solid border-slate-200 rounded-b">
                                     <button
                                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
