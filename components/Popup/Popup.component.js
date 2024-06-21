307069eeb34ea598cc5c4c306dd30c965d7b8e53
husnaabbasi <husnaabbasie@gmail.com>
2024-06-21 20:49:09 +0500

    added component

--- components/Popup/Popup.component.js
+++ components/Popup/Popup.component.js
@@ -4,6 +4,7 @@ const Popup = ({children, title, showPopup, setShowPopup, showFooter = true}) =>
     return (
         <>
             {showPopup ? (
+
                 <>
                     <div
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
@@ -31,15 +32,15 @@ const Popup = ({children, title, showPopup, setShowPopup, showFooter = true}) =>
                                 {/*footer*/}
                                 {showFooter &&
                                     <div
-                                    className="flex items-center justify-end p-4 border-t border-solid border-slate-200 rounded-b">
-                                    <button
-                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                        type="button"
-                                        onClick={() => setShowPopup(false)}
-                                    >
-                                        Close
-                                    </button>
-                                </div>
+                                        className="flex items-center justify-end p-4 border-t border-solid border-slate-200 rounded-b">
+                                        <button
+                                            className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                            type="button"
+                                            onClick={() => setShowPopup(false)}
+                                        >
+                                            Close
+                                        </button>
+                                    </div>
                                 }
                             </div>
                         </div>
