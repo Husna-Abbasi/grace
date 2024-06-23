da1f3fd2ad825f67c5b6e2be2382ee41bf99dbc6
husnaabbasi <husnaabbasie@gmail.com>
2024-06-23 23:45:57 +0500

    added component

--- components/Popup/Popup.component.js
+++ components/Popup/Popup.component.js
@@ -4,7 +4,8 @@ const Popup = ({children, title, showPopup, setShowPopup, showFooter = true}) =>
     return (
         <>
             {showPopup ? (
-                <><div
+                <>
+                    <div
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     >
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
@@ -21,7 +22,7 @@ const Popup = ({children, title, showPopup, setShowPopup, showFooter = true}) =>
                         className="flex justify-center items-center bg-transparent text-slate-700 h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
                     </span>
-                                        
+
                                     </button>
                                 </div>
                                 {/*body*/}
