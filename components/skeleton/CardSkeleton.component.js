0bfeab4130d4c6fc48aecc1120fae8fdb894a722
husnaabbasi <husnaabbasie@gmail.com>
2024-07-07 19:56:52 +0500

    added component

--- components/skeleton/CardSkeleton.component.js
+++ components/skeleton/CardSkeleton.component.js
@@ -70,6 +70,7 @@ const CardSkeletonComponent = ({width, height}) => {
                                 height: '10px',
                                 width: '340px',
                             }}/>
+
                     </div>
                 </div>
                 <div className="flex gap-1 justify-around">
