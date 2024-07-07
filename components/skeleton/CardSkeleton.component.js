966e32b96a6b2e104c7431dc301708e55307d87a
husnaabbasi <husnaabbasie@gmail.com>
2024-07-07 19:58:54 +0500

    added component

--- components/skeleton/CardSkeleton.component.js
+++ components/skeleton/CardSkeleton.component.js
@@ -8,6 +8,7 @@ const CardSkeletonComponent = ({width, height}) => {
     const [avatarShape] = useState('square');
     return (
         <div style={{width, height}}>
+
             <div className="flex flex-col gap-2  md:block mb-4">
                 <Skeleton.Node
                     active={active}
