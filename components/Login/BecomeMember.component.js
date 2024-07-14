e82515ee975697020fcb9e80e822f03d29274ce7
husnaabbasi <husnaabbasie@gmail.com>
2024-07-14 18:34:07 +0500

    added component

--- components/Login/BecomeMember.component.js
+++ components/Login/BecomeMember.component.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ButtonComponent from "../../components/Button/Button.component";
 
-const BecomeMemberComponent = (
-    {
+const BecomeMemberComponent = ({
         title,
         description,
         signInTitle,
