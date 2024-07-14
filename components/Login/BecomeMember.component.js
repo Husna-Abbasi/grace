e23c9b3b1d1d8d5a44e12e840cd9e2a3a3d749ca
husnaabbasi <husnaabbasie@gmail.com>
2024-07-14 18:35:41 +0500

    added component

--- components/Login/BecomeMember.component.js
+++ components/Login/BecomeMember.component.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ButtonComponent from "../../components/Button/Button.component";
 
 const BecomeMemberComponent = ({
-        title,
-        description,
-        signInTitle,
-        signInDescription
-    }) => {
+                                   title,
+                                   description,
+                                   signInTitle,
+                                   signInDescription
+                               }) => {
     return (
         <div className="flex items-center justify-center ">
             <div className="bg-white p-8 w-96 flex flex-col gap-7 rounded">
@@ -19,7 +19,6 @@ const BecomeMemberComponent = ({
                         className="bg-blueS shadow-sm px-[20px] py-[13px] w-full text-white font-semibold text-[18px] rounded-[5px]"
                     />
                 </div>
-
                 <div className=" flex flex-col gap-4">
                     <h1 className="text-xl font-bold text-blues">{signInTitle}</h1>
                     <p className="text-slate-600">{signInDescription}</p>
