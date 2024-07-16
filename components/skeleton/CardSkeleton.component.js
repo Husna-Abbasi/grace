e1ec77c51367479832fc99dd4b56a3491d0cbcc8
husnaabbasi <husnaabbasie@gmail.com>
2024-07-16 21:17:52 +0500

    added component

--- components/skeleton/CardSkeleton.component.js
+++ components/skeleton/CardSkeleton.component.js
@@ -53,6 +53,7 @@ const CardSkeletonComponent = ({width, height}) => {
             {/*//Mobile view*/}
             <div className="flex flex-col gap-1 md:hidden w-full overflow-hidden justify-center mb-4">
                 <div className="flex flex-col gap-2 items-center justify-center">
+
                     <Skeleton.Node
                         active={active}
                         style={{
