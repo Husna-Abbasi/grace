392af24bae1f50d11596c1e5da332a683b28d5d3
husnaabbasi <husnaabbasie@gmail.com>
2024-07-18 22:45:09 +0500

    added component

--- components/skeleton/CardSkeleton.component.js
+++ components/skeleton/CardSkeleton.component.js
@@ -15,7 +15,7 @@ const CardSkeletonComponent = ({width, height}) => {
                         height: '200px',
                         width: '300px',
                     }}/>
-                
+
                 <div className="flex justify-center items-center flex-col pt-2">
                     <Skeleton.Input
                         active={active}
@@ -30,6 +30,7 @@ const CardSkeletonComponent = ({width, height}) => {
                             width: '300px',
                         }}/>
                 </div>
+
                 <div className="flex gap-4 justify-between pt-2">
                     <Skeleton.Button
                         active={active}
