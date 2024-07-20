66c194aecb0943d0ec1087aeab9de6fdc2849d22
husnaabbasi <husnaabbasie@gmail.com>
2024-07-20 17:30:20 +0500

    added component

--- components/Login/Login.component.js
+++ components/Login/Login.component.js
@@ -59,6 +59,7 @@ const LoginComponent = ({onLoginSuccess}) => {
                                 setEmailAddress(e.target.value)
                             }}/>
                     </div>
+
                     <div className="mb-4">
                         <label htmlFor="password" className="block text-slate-700 text-sm font-semibold mb-2">
                             Password
