ba599baa8b83382f546e25bbedb6e080766a54e6
husna <husnaabbasie@gmail.com>
2025-02-24 21:12:46 +0500

    added button

--- components/Button/Button.component.js
+++ components/Button/Button.component.js
@@ -6,7 +6,6 @@ import React from "react";
  * @property {string} label - The label or text content of the button.
  * @property {string} color - Text color of the button.
  * @property {string} [size='w-10'] - Size of the button (default: 'w-10').
- * @property {Function} onClick - Callback function invoked when the button is clicked.
  */
 /**
  * ButtonComponent
