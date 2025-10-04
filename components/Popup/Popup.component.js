d1973bf80fff488f814b420602395921f7e98eee
husna <husnaabbasie@gmail.com>
2025-10-04 23:17:28 +0500

    updated popup

--- components/Popup/Popup.component.js
+++ components/Popup/Popup.component.js
@@ -9,7 +9,6 @@ const Popup = ({children, title, showPopup, setShowPopup, showFooter = true}) =>
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     >
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
-
                             {/*content*/}
                             <div
                                 className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
@@ -19,11 +18,10 @@ const Popup = ({children, title, showPopup, setShowPopup, showFooter = true}) =>
                                     <button
                                         className="bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                                         onClick={() => setShowPopup(false)}>
-                    <span
-                        className="flex justify-center items-center bg-transparent text-slate-700 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-
+                                        <span
+                                            className="flex justify-center items-center bg-transparent text-slate-700 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                            ×
+                                        </span>
                                     </button>
                                 </div>
                                 {/*body*/}
