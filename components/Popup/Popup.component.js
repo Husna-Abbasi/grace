f53031a759aba0e09c3860878c54b9d4c390769c
husna <husnaabbasie@gmail.com>
2026-01-29 17:43:03 +0500

    updated popup

--- components/Popup/Popup.component.js
+++ components/Popup/Popup.component.js
@@ -6,8 +6,7 @@ const Popup = ({children, title, showPopup, setShowPopup, showFooter = true}) =>
             {showPopup ? (
                 <>
                     <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                    >
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
                             {/*content*/}
                             <div
